9d31ffdb5da632a2306142bcafe729bc70bd4ca0
robedgar <116982166+robedgar@users.noreply.github.com>
2022-12-19 11:52:03 +0000

    add PostCss

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,15 +2,35 @@ import dollar from '../assets/icon-dollar.svg';
 import person from '../assets/icon-person.svg';
 const form = () => {
   return (
-    <div className="form">
-      <div className="label-group">
-        <label htmlFor="bill">Bill</label>
-        <input type="number" id="bill" />
-        <img src={dollar} aria-hidden="true" className="icon" />
-      </div>
-      <div className="tip-section">
-        <input type="radio" name="tip" value=".05" />
-        <div className="tip-btn">5%</div>
+    <>
+      <div className="form">
+        <div className="label-group">
+          <label htmlFor="bill">Bill</label>
+          <input type="number" id="bill" />
+          <img src={dollar} aria-hidden="true" className="icon" />
+        </div>
+        <div className="tip-section">
+          <input type="radio" name="tip" value=".05" />
+          <div className="tip-btn">5%</div>
+        </div>
+
+        <div className="tip-amount">
+          <input type="radio" name="tip" value=".05" />
+          <div className="tip-btn">10%</div>
+        </div>
+        <div className="tip-amount">
+          <input type="radio" name="tip" value=".05" />
+          <div className="tip-btn">15%</div>
+        </div>
+        <div className="tip-amount">
+          <input type="radio" name="tip" value=".05" />
+          <div className="tip-btn">25%</div>
+        </div>
+        <div className="tip-amount">
+          <input type="radio" name="tip" value=".05" />
+          <div className="tip-btn">50%</div>
+        </div>
+        <input type="number" className="tip-custom" />
       </div>
       <div className="label-group">
         <div className="label-wrapper">
@@ -20,7 +40,7 @@ const form = () => {
         <input type="number" id="people" />
         <img src={person} aria-hidden="true" className="person" />
       </div>
-    </div>
+    </>
   );
 };
 
